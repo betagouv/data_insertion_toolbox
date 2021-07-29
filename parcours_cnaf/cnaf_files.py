f828ed7deb3cd87658ea3168edfbe0c1e96d60d1
Thomas Guillet <thomas@palya.fr>
2021-07-29 11:45:48 +0200

    Affine les critères de soumis D&D et extrait les données critères en CSV

--- parcours_cnaf/cnaf_files.py
+++ parcours_cnaf/cnaf_files.py
@@ -24,34 +24,60 @@ def process(root):
   return (applications, applicants)
 
 
-ELIGIBLE_SUSPENSION_MOTIVES = ["01", "05", "06", "35", "36"]
-
+# Valeurs :
+# "01" : Ressources trop élévées
+# "02" : Moins 25 ans sans enft/autre person. à charge
+# "03" : Activité non conforme
+# "04" : Titre de séjour non valide
+# "05" : RSA inférieur au seuil
+# "06" : Déclaration Trimestrielle Ressources non fournie
+# "09" : Résidence non conforme
+# "19" : Pas d'isolement
+# "31" : Prestation exclue affil partielle
+# "34" : Régime non conforme
+# "35" : Demande avantage vielliesse absent ou tardif
+# "36" : Titre de séjour absent
+# "44" : Hospitalisation
+# "70" : Action non engagée
+# "78" : Surface pondérée > plafond ou inconnue
+# "84" : Droit éteint
+# "85" : Pas d'allocataire
+# "97" : Bénéficiaire AAH réduite
+# "AB" : Allocataire absent du foyer
+# "CV"  : Attente décision PCG (le droit reste théorique jusqu'au retour)
+# "CG" : Application Sanction -> integré dans les ETATDOSRSA=2
+# "CZ" : Activité antérieure insuffisante
+# "DA" : Activité antérieure absente
+# "DB" : Etudiant rémunération insuff.
+# "DC" : Activité antérieure non conforme
+REAL_ELIGIBLE_SUSPENSION_MOTIVES = ["05", "44", "70"]
+THEORICAL_ELIGIBLE_SUSPENSION_MOTIVES = ["01", "06", "35", "36"]
+ELIGIBLE_SUSPENSION_MOTIVES = REAL_ELIGIBLE_SUSPENSION_MOTIVES + THEORICAL_ELIGIBLE_SUSPENSION_MOTIVES
 
 ERROR_VALUE = "ERROR"
+NOT_APPLICABLE_VALUE = "-1"
 
-def get(node, attribute):
+def get(node, attribute, default_value=ERROR_VALUE):
   try:
     return next(node.iter(attribute)).text
   except Exception as e:
-    return ERROR_VALUE
+    return default_value
 
 
 class Application(object):
   def __init__(self, node):
     super(Application, self).__init__()
     self.rsaApplicationNumber = get(node,"NUMDEMRSA")
+    self.matricule = get(node, "MATRICULE")
     self.statusCode = get(node,"ETATDOSRSA")
-    self.suspensionMotive = get(node,"MOTISUSVERSRSA")
+    self.suspensionMotive = get(node,"MOTISUSVERSRSA", NOT_APPLICABLE_VALUE)
 
   def withDroitsOuvertsEtVersables(self):
     return self.statusCode == "2"
 
-  def withDroitsOuvertsSuspendu(self):
-    return self.statusCode == "4"
-
   def withRights(self):
-    return self.withDroitsOuvertsEtVersables() or (
-      self.withDroitsOuvertsSuspendu() and self.suspensionMotive in ELIGIBLE_SUSPENSION_MOTIVES)
+    return self.withDroitsOuvertsEtVersables() or self.statusCode == "3" or (
+      self.statusCode == "4" and self.suspensionMotive in ELIGIBLE_SUSPENSION_MOTIVES)
 
 
 class Applicant(object):
@@ -59,7 +85,8 @@ class Applicant(object):
     super(Applicant, self).__init__()
     self.application = application
     self.role = get(node,'ROLEPERS')
-    self.topDroitsEtDevoirs = get(node,'TOPPERSDRODEVORSA')
+    self.topDroitsEtDevoirs = get(node,'TOPPERSDRODEVORSA', NOT_APPLICABLE_VALUE)
+    self.topEntrant = get(node,'TOPPERSENTDRODEVORSA', NOT_APPLICABLE_VALUE) == "1"
     self.aid = '{}-{}'.format(self.application.rsaApplicationNumber, self.role)
 
   def withDroitsEtDevoirs(self):
--- parcours_cnaf/main.py
+++ parcours_cnaf/main.py
@@ -7,7 +7,7 @@ import sys
 from tqdm import tqdm
 import xml.etree.ElementTree as ET
 
-from cnaf_files import process, ERROR_VALUE
+from cnaf_files import process, ERROR_VALUE, NOT_APPLICABLE_VALUE
 
 
 def ilen(iterable):
@@ -23,7 +23,9 @@ def get_meta(meta, item):
 
 def main():
   root_path = sys.argv[len(sys.argv)-1]
-  matches = glob.glob("{}{}**{}*.xml*".format(root_path, os.sep, os.sep), recursive=True)
+  matches_base = glob.glob("{}{}**{}*.xml".format(root_path, os.sep, os.sep), recursive=True)
+  matches_gz = glob.glob("{}{}**{}*.xml.gz".format(root_path, os.sep, os.sep), recursive=True)
+  matches = matches_base + matches_gz
   ts = datetime.datetime.now()
 
   data = {
@@ -38,7 +40,9 @@ def main():
     "dossier_erreur": [],
     "personne": [],
     "personne_soumise_DD": [],
-    "personne_erreur": [],
+    "personne_entrante": [],
+    "personne_entrante_erreur": [],
+    "personne_sans_TOPPERSDRODEVORSA": [],
   }
   for m in tqdm(matches):
     start = datetime.datetime.now()
@@ -71,11 +75,42 @@ def main():
     data['dossier_soumis_DD'].append(len([a for a in applications if a.withRights()]))
     data['personne_soumise_DD'].append(len([a for a in applicants if a.withRights()]))
 
+    data['personne_entrante'].append(len([a for a in applicants if a.topEntrant]))
+    data['personne_entrante_erreur'].append(len([a for a in applicants if a.topEntrant and not a.withRights()]))
+
     data['dossier_erreur'].append(len([a for a in applications if a.statusCode == ERROR_VALUE]))
-    data['personne_erreur'].append(len([a for a in applicants if a.topDroitsEtDevoirs == ERROR_VALUE]))
+    data['personne_sans_TOPPERSDRODEVORSA'].append(len([a for a in applicants if a.topDroitsEtDevoirs == NOT_APPLICABLE_VALUE]))
 
     end = datetime.datetime.now()
     data['analysis'].append((end - start).total_seconds())
+
+    if True:
+      data_path = '{}_applicant_extract_{}.csv'.format(m, ts.strftime("%Y-%m-%d_%H-%M-%S"))
+      applicant_data = {
+        'NUMDEMRSA': [],
+        'MATRICULE': [],
+        'id': [],
+        'ROLEPERS': [],
+        'TOPPERSDRODEVORSA': [],
+        'TOPPERSENTDRODEVORSA': [],
+        'ETATDOSRSA': [],
+        'MOTISUSVERSRSA': [],
+        'applicationWithRights': [],
+        'withRights': [],
+        }
+      for a in applicants:
+        applicant_data['NUMDEMRSA'].append(a.application.rsaApplicationNumber)
+        applicant_data['MATRICULE'].append(a.application.matricule)
+        applicant_data['id'].append(a.aid)
+        applicant_data['ROLEPERS'].append(a.role)
+        applicant_data['TOPPERSDRODEVORSA'].append(a.topDroitsEtDevoirs)
+        applicant_data['TOPPERSENTDRODEVORSA'].append(a.topEntrant)
+        applicant_data['ETATDOSRSA'].append(a.application.statusCode)
+        applicant_data['MOTISUSVERSRSA'].append(a.application.suspensionMotive)
+        applicant_data['applicationWithRights'].append(a.application.withRights())
+        applicant_data['withRights'].append(a.withRights())
+      applicant_df = pd.DataFrame.from_dict(applicant_data)
+      applicant_df.to_csv(data_path, index=False)
 
   df = pd.DataFrame.from_dict(data)
   output_path = os.path.join(root_path, 'files_summary_{}.xlsx'.format(ts.strftime("%Y-%m-%d_%H-%M-%S")))
